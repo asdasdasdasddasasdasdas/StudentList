56c10fddc2e4d40c88de2fa7204ab511172cd429
asdasdasdasddasasdasdas <44733011+asdasdasdasddasasdasdas@users.noreply.github.com>
2019-07-25 21:52:00 +0400

    gitignore

--- app/config/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-return [
-    'host' => 'localhost',
-    'name' => 'users',
-    'user' => 'root',                  // Настройка базы данных.
-    'password' => '',
-    'charset' => 'utf8mb4'
-];
